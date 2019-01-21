bd21ee395c8fe7b3d635f152e67eb8e5327ad102
Alina Goaga <goaga.alina@gmail.com>
2019-01-21 16:45:31 +0000

    Equality and inequality can now be tested; work in progress with throwing errors

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -1,7 +1,41 @@
 var square = new Square(2);
 
+//green
 describe('area', () => {
   it('calculates the area of a square', () => {
-    expect(square.area()).toEqual(4);
+    expectEquality(square.area(), 4)
+  })
+})
+
+//red
+describe('area', () => {
+  it('calculates the area of a square', () => {
+    expectEquality(square.area(), 5)
+  })
+})
+
+//green
+describe('area', () => {
+  it('calculates the area of a square', () => {
+    expectInequality(square.area(), 5)
+  })
+})
+
+//red
+describe('area', () => {
+  it('calculates the area of a square', () => {
+    expectInequality(square.area(), 4)
+  })
+})
+
+// describe('constructor', () => {
+//   it('only accepts numbers', () => {
+//     throwAnError(new Square("r"), new Error("argument is not a number"))
+//   })
+// })
+
+describe('constructor', () => {
+  it('only accepts numbers', () => {
+     checkError(new Square("r"));
   })
 })
